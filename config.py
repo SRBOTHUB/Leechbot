050fef74ec5642fef465f60815815856c0d42274
SRBOTHUB <loveshjaat104@gmail.com>
2026-07-14 19:48:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-api_id = 33890563
-api_hash = "0df8da8e970f4a1d96a9f2c2c366e8a6"
-bot_token = "8844011998:AAGpKiiFV2Dckb9PU0fJd8uAE1GgDfWeo7Y"
+import os
+
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
 auth_users = [6023806671]
 sudo_users = [6023806671]
