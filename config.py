057306b6945dfc1f9b00bd5b606d9327b147e35d
SRBOTHUB <loveshjaat104@gmail.com>
2026-07-14 19:34:17 +0530

    Refactor config to use environment variables

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = int(os.environ.get("API_ID", 0))
+api_hash = os.environ.get("API_HASH", "")
+bot_token = os.environ.get("BOT_TOKEN", "")
+auth_users = [6023806671]
+sudo_users = [6023806671]
