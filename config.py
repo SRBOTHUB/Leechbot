891343b7722b24dd99e2ce02cd739d7e4d7738d3
SRBOTHUB <loveshjaat104@gmail.com>
2026-07-14 19:44:45 +0530

    Update API credentials in config.py
    Replaced environment variable assignments with hardcoded values for API credentials and bot token.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = int(os.environ.get("API_ID", 0))
-api_hash = os.environ.get("API_HASH", "")
-bot_token = os.environ.get("BOT_TOKEN", "")
+api_id = 33890563
+api_hash = "0df8da8e970f4a1d96a9f2c2c366e8a6"
+bot_token = "8844011998:AAGpKiiFV2Dckb9PU0fJd8uAE1GgDfWeo7Y"
 auth_users = [6023806671]
 sudo_users = [6023806671]
